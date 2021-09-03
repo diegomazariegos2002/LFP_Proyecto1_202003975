c7d8078fb42f9dcb0b4c12e931ed6e5f28f4d19b
Diego Mazariegos <dmazariegosbarrientos@gmail.com>
2021-09-02 19:37:36 -0600

    Menu provisinal, expresion regular y parte del arbol binario completado

---
+++ Main.py
@@ -0,0 +1,36 @@
+
+
+
+if __name__ == "__main__":
+    while(True):
+        print("\nBienvenido Usuario")
+        print(''''Menú provisional
+        1. Cargar archivo
+        2. Analizar archivo
+        3. Ver reportes
+        4. Seleccionar imagen
+        5. Ver imagen
+        6. Salir''')
+        opcion = int(input("Ingrese una opción: "))
+        if opcion == 1:
+            print("\nEscogió la opción 1 - Cargar archivo")
+            input("Presione Enter para continuar....")
+        elif opcion == 2:
+            print("\nEscogió la opción 2 - Analizar archivo")
+            input("Presione Enter para continuar....")
+        elif opcion == 3:
+            print("\nEscogió la opción 3 - Ver reportes")
+            input("Presione Enter para continuar....")
+        elif opcion == 4:
+            print("\nEscogió la opción 4 - Seleccionar imagen")
+            input("Presione Enter para continuar....")
+        elif opcion == 5:
+            print("\nEscogió la opción 5 - Ver imagen")
+            input("Presione Enter para continuar....")
+        elif opcion == 6:
+            print("\nEscogió la opción 6 - Salir")
+            input("Presione Enter para continuar....")
+            break
+        else:
+            print("\ningrese un digito correcto")
+            input("Presione Enter para continuar....")
